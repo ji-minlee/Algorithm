87880532ff8502e17a0305ebe6ea9a6a45ad8a25
ji-minlee <lee.jimin907@gmail.com>
2022-08-13 14:58:27 +0900

    숫자의 합 구하기

--- dataStructure/sumDigitsOfNumber.java
+++ dataStructure/sumDigitsOfNumber.java
@@ -7,7 +7,7 @@ public class sumDigitsOfNumber {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		//입력값을 String형 변수 sNum에 저장한 후 char[]형 변수로 변환 > String.toCharArray()활용
+		//입력값을 String형 변수 sNum에 저장한 후 char[]형 변수로 변환 > String.toCharArray()
 		String sNum = sc.next();
 		char[] cNum = sNum.toCharArray();
 		
