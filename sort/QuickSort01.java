8d372b5007827226b8337dc5a669ef57d4357c37
ji-minlee <lee.jimin907@gmail.com>
2022-08-16 10:20:52 +0900

    퀵 정렬

--- sort/QuickSort01.java
+++ sort/QuickSort01.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class QuickSort {
+public class QuickSort01 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
